ac74111e8cb38cd0f6e0c5bec140aadf4c0ce624
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2022-12-29 17:16:23 +0700

    make method Insert and add method search in class BinaryTree

--- BinarySearchTree_085/Program.cs
+++ BinarySearchTree_085/Program.cs
@@ -29,6 +29,47 @@ namespace BinarySearchTree_085
         {
             ROOT = null; /*Initializing ROOT to null*/
         }
+        public void Insert(string element)/* Insert a node in the binary search tree */
+        {
+            Node tmp, parent = null, currentNode = null;
+            search(element, ref parent, ref currentNode);
+            if (currentNode != null)/* Check if the node to be inserted already inserted or not */
+            {
+                Console.WriteLine("Duplicate words not allowerd");
+                return;
+            }
+            else /* if the specified node is not present */
+            {
+                tmp = new Node(element, null, null);
+                if (parent == null) /*if the trees is empty */
+                {
+                    ROOT = tmp;
+                }
+                else if (string.Compare(element, parent.info) < 0)
+                {
+                    if (string.Compare(element, parent.info) < 0)
+                        parent.leftchild = tmp;
+                }
+                else
+                {
+                    parent.rightchild = tmp;
+                }
+            }
+        }
+        public void search(string element, ref Node parent, ref Node currentNode)
+        {
+            /*This function searchs the currentNode of the specified Node as well as the current node of its parent */
+            currentNode = ROOT;
+            parent = null;
+            while((currentNode != null) && (currentNode.info != element))
+            {
+                parent = currentNode;
+                if (string.Compare(element, currentNode.info) < 0)
+                    currentNode = currentNode.leftchild;
+                else
+                    currentNode = currentNode.rightchild;
+            }
+        }
     }
     internal class Program
     {
