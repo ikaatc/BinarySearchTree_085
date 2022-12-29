a4e1bb0eb25829756dcb277947f90e8e6d3ed6c4
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2022-12-29 17:04:43 +0700

    add method BinaryTree and Node ROOT in class BinaryTree

--- BinarySearchTree_085/Program.cs
+++ BinarySearchTree_085/Program.cs
@@ -21,6 +21,15 @@ namespace BinarySearchTree_085
             rightchild = r;
         }
     }
+    /* A node class consist of three things, the information, refrences to the right child, and refrences to the left child */
+    class BinaryTree
+    {
+        public Node ROOT;
+        public BinaryTree()
+        {
+            ROOT = null; /*Initializing ROOT to null*/
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
