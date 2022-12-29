4fdd545a34ec7c6626bd3bbb40dc5c0bac0963c4
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2022-12-29 17:00:45 +0700

    add class node

--- BinarySearchTree_085/Program.cs
+++ BinarySearchTree_085/Program.cs
@@ -6,6 +6,14 @@ using System.Threading.Tasks;
 
 namespace BinarySearchTree_085
 {
+    class Node
+    {
+        public string info;
+        public Node leftchild;
+        public Node rightchild;
+
+        //Construcktor for the node class
+    }
     internal class Program
     {
         static void Main(string[] args)
